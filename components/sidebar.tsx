ff58ff50c08fdfc96f073cc8f8b283d86cfa95e0
Ronaldo Santana <ronaldoarsan@gmail.com>
2025-08-19 18:42:28 -0300

    fix: Remove import do componente Button não utilizado no sidebar

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -4,7 +4,6 @@ import { useState } from "react"
 import Link from "next/link"
 import { usePathname } from "next/navigation"
 import { cn } from "@/lib/utils"
-import { Button } from "@/components/ui/button"
 import { LayoutDashboard, CreditCard, Target, TrendingUp, FileText, Settings, Menu, X, Brain } from "lucide-react"
 
 const navigation = [
